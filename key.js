0617e304bee157e386412962f1b4ae430e481fd2
keithinui <55673645+keithinui@users.noreply.github.com>
2020-01-18 10:02:08 +0900

    Update key.js

--- key.js
+++ key.js
@@ -1,3 +1,3 @@
-window.__SKYWAY_KEY__ = '976e956d-7fd6-4e97-a8ea-e6d2d3e31e1a';
+window.__SKYWAY_KEY__ = 'fb5dbf65-0a0d-44cb-851f-076a7550e4ec';
 
 applicationMode = 1;    // 1: Debug mode,   o: Normal mode
